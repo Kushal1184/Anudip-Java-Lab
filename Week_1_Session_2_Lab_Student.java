aac07d8f59f46d1365cca34990df3dd0366352d7
Kushal1184 <131439305+Kushal1184@users.noreply.github.com>
2025-07-13 21:15:43 +0530

    Update and rename Student.java to Week_1_Session_2_Lab_Student.java

---
+++ Week_1_Session_2_Lab_Student.java
@@ -0,0 +1,9 @@
+public class Week_1_Session_2_Lab_Student {
+    private String studentName="John";
+    private String collegeName="Ajeenkya D Y Patil Univercity";
+    private int studentID=11223355;
+
+    public static void main(String[] args) {
+        System.out.println("Successful");
+    }
+}
